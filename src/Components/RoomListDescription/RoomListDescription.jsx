82acd4ad9a68948452ae78c6188f79aef86b03cd
Tim <tmulqueen@premiumbeat.com>
2017-02-19 14:57:08 -0500

    added link to desciprtion component

--- src/Components/RoomListDescription/RoomListDescription.jsx
+++ src/Components/RoomListDescription/RoomListDescription.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export const RoomListDescription = (props) => {
 
-
   return (
-    <div>
+    <Link to={`${props.room.name}` }>
       <h3>{props.room.name}</h3>
       <p>{props.room.desc}</p>
-    </div>
+    </Link>
   )
 }
